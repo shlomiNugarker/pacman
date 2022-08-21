d7d0aaaece4e7c5c29f3d7a8fec3b4c0d857967b
Shlomi Nugarker <98424459+shlomiNugarker@users.noreply.github.com>
2022-08-21 18:44:41 +0300

    Add files via upload

--- js/ghost.js
+++ js/ghost.js
@@ -1,51 +1,61 @@
 'use strict'
-const GHOST_IMG = '<img class="img-mode" src="images/g1.png" />';
+const GHOST_IMG = '<img class="img-mode" src="images/g1.png" />'
 
 var gNum = 1
-var gGhosts;
-var gIntervalGhosts;
-var randColor;
+var gGhosts
+var gIntervalGhosts
+var randColor
 
-
-
-function createGhost(board, idx) {
-   
-    var ghost = {
-        location: {
-            i: 8,
-            j: 8
-        },
-        currCellContent: FOOD_IMG,
-        color: getRandomColor(),
-        idImg: idx + 1
-    }
-    gGhosts.push(ghost);
-    board[ghost.location.i][ghost.location.j] = GHOST_IMG;
-
+function createGhost(board, idx, loc) {
+  var ghost = {
+    location: {
+      i: loc.i,
+      j: loc.j,
+    },
+    currCellContent: FOOD_IMG,
+    color: getRandomColor(),
+    idImg: idx + 1,
+  }
+  gGhosts.push(ghost)
+  board[ghost.location.i][ghost.location.j] = GHOST_IMG
 }
 
 function createGhosts(board) {
-    // 3 ghosts and an interval
-    gGhosts = [];
-    for (var i = 0; i < 3; i++) {
-        createGhost(board, i)
-    }
+  // 3 ghosts and an interval
+  var loc = [
+    {
+      i: 2,
+      j: 2,
+    },
+    {
+      i: 8,
+      j: 1,
+    },
+    {
+      i: 10,
+      j: 17,
+    },
+  ]
+  gGhosts = []
+  for (var i = 0; i < 3; i++) {
+    createGhost(board, i, loc[i])
+  }
 
-    gIntervalGhosts = setInterval(moveGhosts, 500)
+  gIntervalGhosts = setInterval(moveGhosts, 500)
 }
 
 function moveGhosts() {
-    // console.table(gBoard)
-    // loop through ghosts
-    for (var i = 0; i < gGhosts.length; i++) {
-        var ghost = gGhosts[i];
-        moveGhost(ghost)
-    }
+  // console.table(gBoard)
+  // loop through ghosts
+  for (var i = 0; i < gGhosts.length; i++) {
+    var ghost = gGhosts[i]
+    moveGhost(ghost)
+  }
 }
 
 // function randomPolicy(ghost) {
 //     var moveDiff = getMoveDiff()
-    
+
 //     var nextLocation = {
 //         i: ghost.location.i + moveDiff.i,
 //         j: ghost.location.j + moveDiff.j,
@@ -54,89 +64,91 @@ function moveGhosts() {
 // }
 
 function moveGhost(ghost) {
-    // console.log('ghost.location', ghost.location)
-    // figure out moveDiff, nextLocation, nextCell
+  // console.log('ghost.location', ghost.location)
+  // figure out moveDiff, nextLocation, nextCell
 
-    // var moveDiff = getMoveDiff()
-    var moveDiff = !gPacman.isSuper?  getAttackPac(currPosPacman, ghost.location): getMoveDiff(currPosPacman)
-    var nextLocation = {
-        i: ghost.location.i + moveDiff.i,
-        j: ghost.location.j + moveDiff.j,
+  var moveDiff = getMoveDiff()
+  var moveAttack = getAttackPac(gPacman.location, ghost.location)
+  var isAttack = true
+  //   var isAttack = !gPacman.isSuper
+  isAttack
+    ? getAttackPac(currPosPacman, ghost.location)
+    : getMoveDiff(currPosPacman)
+  var nextLocation = {
+    i: ghost.location.i + moveAttack.i,
+    j: ghost.location.j + moveAttack.j,
+  }
+
+  var nextCell = gBoard[nextLocation.i][nextLocation.j]
+  // console.log('nextCell', nextCell)
+  // return if cannot move
+  if (nextCell === WALL_IMG) return
+  if (nextCell === GHOST_IMG) return
+  if (nextCell === CHERRY_IMG) return
+  // hitting a pacman?  call gameOver
+  if (nextCell === PACMAN_IMG) {
+    if (gPacman.isSuper) return
+    else {
+      gameOver()
+      return
     }
+  }
 
-    // console.log('nextLocation', nextLocation)
+  // moving from corrent position:
+  // update the model
+  gBoard[ghost.location.i][ghost.location.j] = ghost.currCellContent
+  // update the DOM
+  renderCell(ghost.location, ghost.currCellContent)
 
-    var nextCell = gBoard[nextLocation.i][nextLocation.j]
-    // console.log('nextCell', nextCell)
-    // return if cannot move
-    if (nextCell === WALL_IMG) return
-    if (nextCell === GHOST_IMG) return
-    if (nextCell === CHERRY_IMG) return
-    // hitting a pacman?  call gameOver
-    if (nextCell === PACMAN_IMG) {
-        if(gPacman.isSuper)  return
-        else {
-            gameOver();
-            return
-        }
-        
-    }
+  // Move the ghost to new location
+  // update the model
+  ghost.location = {
+    i: nextLocation.i,
+    j: nextLocation.j,
+  }
+  ghost.currCellContent = nextCell
+  gBoard[ghost.location.i][ghost.location.j] = GHOST_IMG
+  // update the DOM
 
-    // moving from corrent position:
-    // update the model
-    gBoard[ghost.location.i][ghost.location.j] = ghost.currCellContent
-    // update the DOM
-    renderCell(ghost.location, ghost.currCellContent)
-
-    // Move the ghost to new location
-    // update the model
-    ghost.location = {
-        i: nextLocation.i,
-        j: nextLocation.j
-    }
-    ghost.currCellContent = nextCell
-    gBoard[ghost.location.i][ghost.location.j] = GHOST_IMG
-    // update the DOM
-
-    renderCell(ghost.location, getGhostHTML(ghost, gPacman.isSuper))
+  renderCell(ghost.location, getGhostHTML(ghost, gPacman.isSuper))
 }
 
 function getGhostHTML(ghost, isSuper) {
-    if(isSuper) {
-        return `<span style="color: red;"><img class="img-mode" src="images/ghost.png" /></span>`
-    }
-    else {
-        return `<span style="color:;"><img class="img-mode" src="images/g${ghost.idImg}.png" /></span>`
-    }
-    
+  if (isSuper) {
+    return `<span style="color: red;"><img class="img-mode" src="images/ghost.png" /></span>`
+  } else {
+    return `<span style="color:;"><img class="img-mode" src="images/g${ghost.idImg}.png" /></span>`
+  }
 }
 
 function getMoveDiff(currPosPac) {
-    var randNum = getRandomIntInclusive(1, 100);
-    currPosPac = gPacman.location
-    console.log(currPosPac);
-    if (randNum <= 25) {
-        return { i: 0, j: 1 }
-    } else if (randNum <= 50) {
-        return { i: -1, j: 0 }
-    } else if (randNum <= 75) {
-        return { i: 0, j: -1 }
-    } else {
-        return { i: 1, j: 0 }
-    }
+  var randNum = getRandomIntInclusive(1, 100)
+  currPosPac = gPacman.location
+  console.log(currPosPac)
+  if (randNum <= 25) {
+    return { i: 0, j: 1 }
+  } else if (randNum <= 50) {
+    return { i: -1, j: 0 }
+  } else if (randNum <= 75) {
+    return { i: 0, j: -1 }
+  } else {
+    return { i: 1, j: 0 }
+  }
 }
 
 function getAttackPac(pacPos, ghostPos) {
-    console.log(pacPos, ghostPos);
- 
-    // if(!gPacman.isSuper) {
-        if(pacPos.j > ghostPos.j) return {i: 0, j: 1}
-        if(pacPos.j === ghostPos.j && pacPos.i > ghostPos.i) return {i:1, j: 0}
-        if(ghostPos.j > pacPos.j) return {i: 0, j: -1}
-        if(ghostPos.i > pacPos.i) return {i: -1, j: 0}
-        
-    // }
-    // else
+  console.log('getAttackPac')
+  console.log(pacPos, ghostPos)
+  var posToAttack = null
 
+  //   if (gPacman.isSuper) {
+  if (pacPos.j > ghostPos.j) posToAttack = { i: 0, j: 1 }
+  if (pacPos.j === ghostPos.j && pacPos.i > ghostPos.i)
+    posToAttack = { i: 1, j: 0 }
+  if (ghostPos.j > pacPos.j) posToAttack = { i: 0, j: -1 }
+  if (ghostPos.i > pacPos.i) posToAttack = { i: -1, j: 0 }
+  //   }
+  console.log({ posToAttack })
+  return posToAttack
+  // else
 }
-
--- js/pacman.js
+++ js/pacman.js
@@ -1,133 +1,133 @@
 'use strict'
 var PACMAN_IMG = `<img class="img-mode pacman-r" src="images/pacman.gif" />`
 
-var gPacman;
+var gPacman = { location: { i: 7, j: 10 } }
 var killedGhosts = []
 var idx = -1
 
-var dir ; //diraction class of img (getNextLocation)
+var dir //diraction class of img (getNextLocation)
 
-var emptyLocations = [];
-var currPosPacman = {i: 4, j: 6}
+var emptyLocations = []
+var currPosPacman = { i: 4, j: 6 }
 
-var powerSound = new Audio('sound/power.wav');
+var powerSound = new Audio('sound/power.wav')
 var moveSound = new Audio('sound/move.wav')
 
 function createPacman(board) {
-    gPacman = {
-        location: {
-            i: 3,
-            j: 3
-        },
-        isSuper: false,
-        
-    }
-    board[gPacman.location.i][gPacman.location.j] = PACMAN_IMG;
+  gPacman = {
+    location: {
+      i: 7,
+      j: 10,
+    },
+    isSuper: false,
+  }
+  board[gPacman.location.i][gPacman.location.j] = PACMAN_IMG
 }
 
 function movePacman(ev) {
-    if(ev.code === 'Space' && !gGame.isOn) init()
-    if (!gGame.isOn ) return
+  if (ev.code === 'Space' && !gGame.isOn) init()
+  if (!gGame.isOn) return
 
-    moveSound.play()
-    
-    // use getNextLocation(), nextCell
-    var nextLocation = getNextLocation(ev)
-    currPosPacman = nextLocation
-    // console.log('nextLocation', nextLocation)
-    var nextCell = gBoard[nextLocation.i][nextLocation.j]
+  //   moveSound.play()
 
-    //diraction of pacman
-    
-    PACMAN_IMG = `<img class="img-mode pacman-${dir}" src="images/pacman.gif" />` // curr heading
+  // use getNextLocation(), nextCell
+  var nextLocation = getNextLocation(ev)
+  currPosPacman = nextLocation
+  // console.log('nextLocation', nextLocation)
+  var nextCell = gBoard[nextLocation.i][nextLocation.j]
 
-    if (nextCell === WALL_IMG) return
-    // hitting a ghost?  call gameOver
-    if (nextCell === GHOST_IMG) {
-        if(gPacman.isSuper) {
-            for(var i = 0; i < gGhosts.length; i++){
-                if(gGhosts[i].location.i === nextLocation.i &&
-                    gGhosts[i].location.j === nextLocation.j) {
-                        killedGhosts.push(...gGhosts.splice(i, 1))
-                    }
-            }
-            if(killedGhosts.currCellContent === FOOD_IMG) updateScore(1)
+  //diraction of pacman
+
+  PACMAN_IMG = `<img class="img-mode pacman-${dir}" src="images/pacman.gif" />` // curr heading
+
+  if (nextCell === WALL_IMG) return
+  // hitting a ghost?  call gameOver
+  if (nextCell === GHOST_IMG) {
+    if (gPacman.isSuper) {
+      for (var i = 0; i < gGhosts.length; i++) {
+        if (
+          gGhosts[i].location.i === nextLocation.i &&
+          gGhosts[i].location.j === nextLocation.j
+        ) {
+          killedGhosts.push(...gGhosts.splice(i, 1))
         }
-        else {
-          gameOver();
-        document.querySelector('.msg').innerText = 'Game Over \n Click "space" to play again'
-        return  
-        }
+      }
+      if (killedGhosts.currCellContent === FOOD_IMG) updateScore(1)
+    } else {
+      gameOver()
+      document.querySelector('.msg').innerText =
+        'Game Over \n Click "space" to play again'
+      return
     }
-    if(nextCell === CHERRY_IMG){
-        powerSound.play()
-        gGame.cherry += 10
-    } 
+  }
+  if (nextCell === CHERRY_IMG) {
+    powerSound.play()
+    gGame.cherry += 10
+  }
 
-    if(killedGhosts[0] && !gPacman.isSuper) {
-        for(var i = 0; i < killedGhosts.length; i++) {
-            gGhosts.push(killedGhosts[i])
-
-        }
-        killedGhosts = []
+  if (killedGhosts[0] && !gPacman.isSuper) {
+    for (var i = 0; i < killedGhosts.length; i++) {
+      gGhosts.push(killedGhosts[i])
     }
-  
-    if (nextCell === FOOD_IMG) {
-        updateScore(1)
-        emptyLocations.push(nextLocation)
+    killedGhosts = []
+  }
 
-    }
-    if(nextCell === POWER_IMG) {
-        if(gPacman.isSuper) return
-        powerSound.play()
-        gPacman.isSuper = true
-        setTimeout(()=> {
-            gPacman.isSuper = false;
-        }, 7000)
-    }
+  if (nextCell === FOOD_IMG) {
+    updateScore(1)
+    emptyLocations.push(nextLocation)
+  }
+  if (nextCell === POWER_IMG) {
+    if (gPacman.isSuper) return
+    powerSound.play()
+    gPacman.isSuper = true
+    setTimeout(() => {
+      gPacman.isSuper = false
+    }, 7000)
+  }
 
-    // moving from corrent position:
-    // update the model
-    gBoard[gPacman.location.i][gPacman.location.j] = EMPTY
-    // update the DOM
-    renderCell(gPacman.location, EMPTY)
+  // moving from corrent position:
+  // update the model
+  gBoard[gPacman.location.i][gPacman.location.j] = EMPTY
+  // update the DOM
+  renderCell(gPacman.location, EMPTY)
 
-    // Move the pacman to new location
-    // update the model
-    gPacman.location = {
-        i: nextLocation.i,
-        j: nextLocation.j
-    }
-    gBoard[gPacman.location.i][gPacman.location.j] = PACMAN_IMG
-    // update the DOM
-    renderCell(gPacman.location, PACMAN_IMG)
+  // Move the pacman to new location
+  // update the model
+  gPacman.location = {
+    i: nextLocation.i,
+    j: nextLocation.j,
+  }
+  gBoard[gPacman.location.i][gPacman.location.j] = PACMAN_IMG
+  // update the DOM
+  renderCell(gPacman.location, PACMAN_IMG)
 }
 
 function getNextLocation(keyboardEvent) {
-    var nextLocation = {
-        i: gPacman.location.i,
-        j: gPacman.location.j
-    }
-    console.log(currPosPacman);
+  var nextLocation = {
+    i: gPacman.location.i,
+    j: gPacman.location.j,
+  }
+  console.log(currPosPacman)
 
-    switch (keyboardEvent.code) {
-        case 'ArrowUp':
-            nextLocation.i--
-            dir = 'u'
-            break;
-        case 'ArrowDown':
-            nextLocation.i++
-            dir = 'd'
-            break;
-        case 'ArrowLeft':
-            nextLocation.j--
-            dir = 'l'
-            break;
-        case 'ArrowRight':
-            nextLocation.j++
-            dir = 'r';
-            break;
-        default: return null
-    }
-    return nextLocation;
+  switch (keyboardEvent.code) {
+    case 'ArrowUp':
+      nextLocation.i--
+      dir = 'u'
+      break
+    case 'ArrowDown':
+      nextLocation.i++
+      dir = 'd'
+      break
+    case 'ArrowLeft':
+      nextLocation.j--
+      dir = 'l'
+      break
+    case 'ArrowRight':
+      nextLocation.j++
+      dir = 'r'
+      break
+    default:
+      return null
+  }
+  return nextLocation
 }
